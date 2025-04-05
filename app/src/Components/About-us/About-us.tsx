e409617954b84ff2d4ef41226e021bea08007f9e
astheeknadig <astheek248@gmail.com>
2025-04-05 13:05:05 +0530

    feat: about-us & store-info section

---
+++ app/src/Components/About-us/About-us.tsx
@@ -0,0 +1,14 @@
+import './About-us.scss';
+
+export default function AboutUs() {
+    return (
+        <div className="about-us-container">
+            <div className="about-us">
+                <p>
+                    Welcome to our hardware catalog! We are dedicated to providing high-quality hardware products
+                    with detailed specifications and images. Our goal is to make hardware shopping easy and informative.
+                </p>
+            </div>
+        </div>
+    );
+};
